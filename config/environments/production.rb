6b22dbf9a628786b2b2c360f1650c4418f43ba45
Ryan Bruno <ryan.bruno727@gmail.com>
2013-08-26 20:24:25 -0400

    linked production.rb to url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Omrails::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # In production, :host should be set to the actual host of your application
-  config.action_mailer.default_url_options = { :host => 'http://arcane-atoll-5072.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://arcane-atoll-5072.herokuapp.com' }
 
 
 end
