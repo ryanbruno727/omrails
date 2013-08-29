7c12d24867a4763a5f1bbca482dd3b2721fa3c99
Ryan Bruno <ryan.bruno727@gmail.com>
2013-08-28 22:12:52 -0400

    added user asociation to pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,12 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
+  attr_accessible :destroy
+  attr_accessible :show
+  attr_accessible :edit
 
   validates :description, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true
+  
 end
